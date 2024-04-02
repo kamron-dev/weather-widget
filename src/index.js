97e9af3425c7124fa54680e78a488d619a37c86f
kamron-dev <k.kamron@yadnex.ru>
2024-04-02 20:16:01 +0500

    fetchWeather and extractInfo working functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
-// console.log("working!");
 const myAPIkey = "732d76b905324f7288a105918242803";
+
 async function fetchWeather(apiKey, city) {
     try {
         const responce = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`);
         const data = await responce.json();
         console.log(data)
-        console.log(data.current.temp_c, data.location.name, data.location.country)
+        // console.log(data.current.temp_c, data.location.name, data.location.country)
+        return data;
     } catch (error) {
         console.error("Error!:", error);
+        return null;
     };
     
 };
+fetchWeather(myAPIkey, "Tashkent")
+    .then(data => {
+        console.log(extractInfo(data));
+    })
 
-fetchWeather(myAPIkey, "Tashkent");
+function extractInfo(data) {
+    const { name, country } = data.location;
+    const { temp_c } = data.current;
+    return {
+        location: {name, country},
+        temp_c
+    }
+};
+
+
+// let answer = fetchWeather(myAPIkey, "Tashkent");
+// console.log(answer)
+
