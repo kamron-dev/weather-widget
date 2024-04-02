6d79aac129aaaa8ceb69a19deb10507ed72800d1
kamron-dev <k.kamron@yadnex.ru>
2024-04-02 19:39:48 +0500

    created function that logs the weather to the console

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("working!");
+// console.log("working!");
+const myAPIkey = "732d76b905324f7288a105918242803";
+async function fetchWeather(apiKey, city) {
+    try {
+        const responce = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`);
+        const data = await responce.json();
+        console.log(data)
+        console.log(data.current.temp_c, data.location.name, data.location.country)
+    } catch (error) {
+        console.error("Error!:", error);
+    };
+    
+};
+
+fetchWeather(myAPIkey, "Tashkent");
