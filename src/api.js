60f6567baf6f2daa70d635b61ae9e59566671d70
kamron-dev <k.kamron@yadnex.ru>
2024-04-03 19:14:06 +0500

    working properly

--- src/api.js
+++ src/api.js
@@ -12,12 +12,12 @@ export async function fetchWeather(apiKey, city) {
     
 };
 
-export async function fetchGIF(word = "cat") {
+export async function fetchGIF(word = "cat", container) {
     
     try {
         const responce = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=EWfxxdqIcrSsyR0l7DRB0foiZ7lGBAUr&s=${word}`, { mode: "cors" });
         const responceData = await responce.json();
-        img.src = responceData.data.images.original.url;
+        container.style.backgroundImage = `url(${responceData.data.images.original.url})`;
     } catch (error) {
         console.error("Network responce error:", error);
     }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./style.css";
-import { fetchWeather } from "./api";
+import { fetchWeather, fetchGIF } from "./api";
 
 const inputCityName = document.querySelector("#cityName");
 const searchButton = document.querySelector("#citySubmit");
@@ -8,18 +8,20 @@ const weatherCard = document.querySelector("#card");
 const cityName = document.querySelector("#cityH2");
 const temperature = document.querySelector("#temp_C");
 const description = document.querySelector("#desc");
+const icon = document.querySelector("#icon");
 
 const myAPIkey = "732d76b905324f7288a105918242803";
 
 searchButton.addEventListener("click", () => {
     fetchWeather(myAPIkey, inputCityName.value)
         .then(data => {
+            console.log(data);
             data = extractInfo(data);
             cityName.textContent = data.location.name;
             temperature.textContent = data.temp_c + "\u00B0C";
             description.textContent = data.condition.text;
-            // console.log(data)
-            
+            icon.src = data.condition.icon;
+            fetchGIF(data.condition.text, weatherCard);
             
         })
     inputCityName.value = "";
